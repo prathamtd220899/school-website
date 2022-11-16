b0c5b7a5c04a86c8c3d98c6749d97b3382d7c112
Prathamesh <prathameshdhasade2015@gmail.com>
2022-11-16 17:00:53 +0530

    school website commit

---
+++ javascript/index.js
@@ -0,0 +1,38 @@
+// change navbar styles on scroll
+window.addEventListener('scroll',() =>{
+    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0)
+})
+
+// show/hide faq answer
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click',() => {
+        faq.classList.toggle('open')
+
+        //change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if (icon.className === 'bi bi-plus') {
+            icon.className = 'bi bi-dash'
+        }else{
+            icon.className = 'bi bi-plus'
+        }
+    })
+})
+
+// show/hide nav menu
+const menu = document.querySelector('.nav__menu');
+const openBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+openBtn.addEventListener('click',() => {
+    menu.style.display = 'flex';
+    closeBtn.style.display = 'inline-block';
+    openBtn.style.display = 'none';
+})
+
+closeBtn.addEventListener('click',() => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    openBtn.style.display = 'inline-block';
+})
